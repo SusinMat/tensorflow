fe5439215b62faa5c0a4522f27b26c22b62afdf2
SusinMat <57694766+SusinMat@users.noreply.github.com>
2019-06-04 18:51:33 -0300

    Remove 'out_s' from Dragunov

--- tensorflow/core/user_ops/dragunov.cc
+++ tensorflow/core/user_ops/dragunov.cc
@@ -3,20 +3,6 @@
 #include "tensorflow/core/framework/op_kernel.h"
 
 namespace tensorflow {
-
-Status DragunovShape(shape_inference::InferenceContext* c) {
-  int out_s;
-  shape_inference::DimensionHandle output_channels;
-  shape_inference::ShapeHandle dims_f;
-  shape_inference::ShapeHandle output_shape;
-  TF_RETURN_IF_ERROR(c->WithRank(c->input(3), 4, &dims_f));
-  TF_RETURN_IF_ERROR(c->Multiply(c->Dim(dims_f, 0), c->Dim(dims_f, 3), &output_channels));
-  TF_RETURN_IF_ERROR(c->GetAttr("out_s", &out_s));
-  output_shape = c->MakeShape({c->MakeDim(1), c->MakeDim(out_s), c->MakeDim(out_s), output_channels});
-  c->set_output(0, output_shape);
-  return Status::OK();
-}
-
 #if 0
 Status ConvShape(shape_inference::InferenceContext* c) {
   string data_format_str, filter_format_str;
@@ -128,6 +114,55 @@ Status ConvShape(shape_inference::InferenceContext* c) {
 }
 #endif
 
+Status DragunovShape(shape_inference::InferenceContext* c) {
+  int padding, stride_h, stride_w,
+      out_height, out_width, in_height, in_width, filter_height, filter_width;
+  TF_RETURN_IF_ERROR(c->GetAttr("padding", &padding));
+  TF_RETURN_IF_ERROR(c->GetAttr("stride_h", &stride_h));
+  TF_RETURN_IF_ERROR(c->GetAttr("stride_w", &stride_w));
+  // For the SAME(1) padding, the output height and width are computed as:
+  //
+  // out_height = ceil(float(in_height) / float(strides[1]))
+  // out_width  = ceil(float(in_width) / float(strides[2]))
+  //
+  // And
+  //
+  // For the VALID(2) padding, the output height and width are computed as:
+  //
+  // out_height = ceil(float(in_height - filter_height + 1) / float(strides[1]))
+  // out_width  = ceil(float(in_width - filter_width + 1) / float(strides[2]))
+
+  shape_inference::DimensionHandle output_channels;
+  shape_inference::ShapeHandle dims_f;
+#if 1
+  shape_inference::ShapeHandle input_shape;
+  shape_inference::ShapeHandle dims_z;
+  TF_RETURN_IF_ERROR(c->WithRank(c->input(2), 6, &dims_z));
+  TF_RETURN_IF_ERROR(c->WithRank(c->input(0), 4, &input_shape));
+  in_height = c->Value(c->Dim(input_shape, 1));
+  in_width = c->Value(c->Dim(input_shape, 2));
+  filter_height = c->Value(c->Dim(dims_z, 1));
+  filter_width = c->Value(c->Dim(dims_z, 2));
+  if (padding == 1) {
+    out_height = (int)ceilf(float(in_height) / float(stride_h));
+    out_width  = (int)ceilf(float(in_width) / float(stride_w));
+  } else if (padding == 2) {
+    out_height = (int)ceilf(float(in_height - filter_height + 1) / float(stride_h));
+    out_width  = (int)ceilf(float(in_width - filter_width + 1) / float(stride_w));
+  } else {
+    out_height = -1;
+    out_width = -1;
+  }
+#endif
+  shape_inference::ShapeHandle output_shape;
+  TF_RETURN_IF_ERROR(c->WithRank(c->input(3), 4, &dims_f));
+  TF_RETURN_IF_ERROR(c->Multiply(c->Dim(dims_f, 0), c->Dim(dims_f, 3), &output_channels));
+
+  output_shape = c->MakeShape({c->MakeDim(1), c->MakeDim(out_height), c->MakeDim(out_width), output_channels});
+  c->set_output(0, output_shape);
+  return Status::OK();
+}
+
 REGISTER_OP("Dragunov")
     .Input("input: float")
     .Input("filter_c: float")
@@ -137,8 +172,6 @@ REGISTER_OP("Dragunov")
     .Attr("stride_h: int")
     .Attr("stride_w: int")
     .Attr("padding: int") // TODO: enum class Padding: int8_t { UNKNOWN = 0, SAME, VALID };
-    // TODO: probably easier to just read shape from input
-    .Attr("out_s: int")
     .SetShapeFn(DragunovShape);
 
 
